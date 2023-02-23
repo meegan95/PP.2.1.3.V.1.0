774af6ca0920f6611854173b81f2f54556dd5c2c
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-02-23 13:32:23 +0300

    v.1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Ссылки на бины HelloWorld одинаковые? " + (bean == bean2));
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Ссылки на бины Cat одинаковые? " + (catBean == catBean2));
     }
 }
